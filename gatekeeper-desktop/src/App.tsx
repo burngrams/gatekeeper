a69b2a0c9c450f6321ac14932167be11dbb17956
Peter Griffin <thebird@istheword.com>
2023-05-17 06:34:13 +0300

    tabs and shit for everything

--- gatekeeper-desktop/src/App.tsx
+++ gatekeeper-desktop/src/App.tsx
@@ -14,28 +14,89 @@ async function getQRCode(user: string) {
 function encodeSVGAsDataURI(svg: string) {
   return `data:image/svg+xml;utf8,${encodeURIComponent(svg)}`
 }
+
+// TODO extract
+function Section(props: { title: string, children: React.ReactNode, id: string }) {
+  return (
+    <details id={props.id}>
+      <summary>
+        <span className="icon">👇</span>
+        <h2>{props.title}</h2>
+      </summary>
+      <p>
+        {props.children}
+      </p>
+    </details>
+  )
+}
 function App() {
   const [user, setName] = useState("");
   const [qrCode, setQRCode] = useState<string | null>(null);
+  const [gatekeepers, setGatekeepers] = useState<Gatekeeper[]>([]);
 
   return (
     <div className="container">
       <h1>Welcome to Gatekeeper!</h1>
       <h2></h2>
-      <h2>Manage Data</h2>
-      <div>
+      <Section title="Manage Data" id="manage-data">
         TBD - manage data
         <ul>
           <li>drag and drop JSON feature to here</li>
           <li>show some statistics on list of attendees</li>
           <li>add text attendees can be viewed in JSON / CSV at :path_to_json</li>
         </ul>
-      </div>
-      <details>
-        <summary>
-          <span className="icon">👇</span>
-          <h2>Create Test Tickets</h2>
-        </summary>
+      </Section>
+
+      <Section title="Create Login" id="create-credentials">
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            setQRCode(await getQRCode(user));
+          }}
+        >
+          <input
+            id="login-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a user..."
+          />
+          <button type="submit">generate QR</button>
+        </form>
+
+        {qrCode && <img src={encodeSVGAsDataURI(qrCode)} alt="QR Code" />}
+      </Section>
+      <Section title="Gatekeepers List" id="gatekeepers-list">
+        <div className="row">
+          {gatekeepers.map((gatekeeper) => (
+            <div className="col-4" key={gatekeeper.id}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{gatekeeper.name}</h5>
+                  <p className="card-text">
+                    <strong>Phone:</strong> {gatekeeper.phone}
+                    <br />
+                    <strong>Address:</strong> {gatekeeper.address}
+                  </p>
+                  <div className="btn-group" role="group">
+                    <Link
+                      to={`/edit-gatekeeper/${gatekeeper.id}`}
+                      className="btn btn-outline-primary"
+                    >
+                      Edit
+                    </Link>
+                    <button
+                      onClick={() => deleteGatekeeper(gatekeeper.id)}
+                      className="btn btn-outline-danger"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Section>
+      <Section id="test-tickets" title="Create Test Tickets">
         <p>
           <form
             style={{ display: "flex", flexDirection: "column" }}
@@ -55,32 +116,7 @@ function App() {
             <img id="generate-test-ticket" />
           </form>
         </p>
-      </details>
-      <h2>Credential Generator</h2>
-      <div className="row">
-        <div className="row">
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault();
-              setQRCode(await getQRCode(user));
-            }}
-          >
-            <input
-              id="login-input"
-              onChange={(e) => setName(e.currentTarget.value)}
-              placeholder="Enter a user..."
-            />
-            <button type="submit">generate QR</button>
-          </form>
-        </div>
-        <div className="row">
-          {qrCode && <img src={encodeSVGAsDataURI(qrCode)} alt="QR Code" />}
-        </div>
-      </div>
-
-      <h2>Gatekeepers List</h2>
-      <div className="row">
-      </div>
+      </Section>
     </div>
   );
 }
