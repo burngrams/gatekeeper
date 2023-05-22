41dce10124eca96e453dc9d5779e7b383cbaef11
Peter Griffin <thebird@istheword.com>
2023-05-23 02:30:49 +0300

    Revert "maybe by pinging we get bonjour like behaviour"
    This reverts commit 900f26fea7d09407c49bcf0f7c9b2852e5fcee55.

--- gatekeeper-app/src/lib/trpcReact.ts
+++ gatekeeper-app/src/lib/trpcReact.ts
@@ -2,45 +2,14 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
 import { createTRPCReact } from '@trpc/react-query'
 import type { AppRouter } from 'gatekeeper-desktop/lib'
 import superjson from 'superjson'
-import * as Network from 'expo-network'
 
 export const trpcReact = createTRPCReact<AppRouter>()
 
-export let trpc = createTRPCProxyClient<AppRouter>({
+export const trpc = createTRPCProxyClient<AppRouter>({
   transformer: superjson,
   links: [
     httpBatchLink({
-      url: 'http://localhost:3000',
+      url: 'http://10.101.93.176:3000',
     }),
   ],
 })
-
-// get network ip address from expo-network
-// https://docs.expo.io/versions/latest/sdk/network/#networkgetipaddressasync
-;(async () => {
-  const ipAddress = '10.0.0.30'
-  const [o1, o2, o3, octet] = ipAddress.split('.')
-  // run from second octet to octet, try to connect to the first one that works
-  for (let i = 2; i <= +octet; i++) {
-    const url = `http://${o1}.${o2}.${o3}.${i}:3000`
-    try {
-      const attemptTrpc = createTRPCProxyClient<AppRouter>({
-        transformer: superjson,
-        links: [
-          httpBatchLink({
-            url,
-          }),
-        ],
-      })
-      const res = await trpc.ping.query()
-      console.log('trying ip', url, res)
-      if (res === 'pong') {
-        console.log('connected to', url)
-        trpc = attemptTrpc
-        break
-      }
-    } catch (e) {
-      console.log('failed to connect to', e, url)
-    }
-  }
-})()
--- gatekeeper-desktop/lib/routers/index.ts
+++ gatekeeper-desktop/lib/routers/index.ts
@@ -1,8 +1,7 @@
 import { helloWorldRouter } from './hello-world'
 import { tickets } from './tickets.router'
 import { t } from '../trpc/t'
-import { publicProcedure } from '../trpc'
 
-export const appRouter = t.router({ tickets, ping: publicProcedure.query(() => 'pong' as const) })
+export const appRouter = t.router({ tickets })
 
 export type AppRouter = typeof appRouter
