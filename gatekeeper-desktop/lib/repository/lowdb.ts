48abd8822437ae8fb121a71ae963f1046fb6723f
Peter Griffin <thebird@istheword.com>
2023-05-22 17:22:36 +0300

    for the record, lowdb coolo technique

--- gatekeeper-desktop/lib/repository/lowdb.ts
+++ gatekeeper-desktop/lib/repository/lowdb.ts
@@ -4,8 +4,8 @@ import { fileURLToPath } from 'url'
 
 import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb/node'
-import type { DatabaseModel } from './models'
-import { DatabaseLayer } from './repository/types'
+import type { DatabaseModel } from '../models'
+import { DatabaseLayer } from './types'
 
 // db.json file path
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -14,10 +14,4 @@ const file = join(__dirname, 'db.json')
 // Configure lowdb to write data to JSON file
 const adapter = new JSONFile<DatabaseModel>(file)
 const defaultData: DatabaseModel = { tickets: [], gatekeepers: [], shifts: [], participants: [] }
-const db: DatabaseLayer = new Low(adapter, defaultData)
-
-// Read data from JSON file, this will set db.data content
-// If JSON file doesn't exist, defaultData is used instead
-await db.read()
-
-export default db
+export const db: DatabaseLayer = new Low(adapter, defaultData)
