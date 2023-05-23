0f0baa766bff62d0481f3af2a186b55187a6ae7e
Peter Griffin <thebird@istheword.com>
2023-05-24 02:52:13 +0300

    refactor: merge server and index files

--- gatekeeper-desktop/electron/server.ts
+++ gatekeeper-desktop/electron/server.ts
@@ -1,9 +1,12 @@
-import { createHTTPServer } from '@trpc/server/adapters/standalone'
+import path from 'path'
+import { app, BrowserWindow, ipcMain } from 'electron'
+import { createIPCHandler } from 'electron-trpc/main'
 import { appRouter } from '../lib'
+import { createHTTPServer } from '@trpc/server/adapters/standalone'
 import { generateContext } from '../lib/trpc/context'
 import { getDatabase } from '../lib/repository/lowdb'
 
-export async function createServer() {
+async function createServer() {
   const lowdb = await getDatabase()
   const createContext = await generateContext(lowdb)
 
@@ -15,3 +18,33 @@ export async function createServer() {
   server.listen(3000)
   console.log('Listening on port 3000...')
 }
+
+process.env.DIST = path.join(__dirname, '../dist')
+process.env.PUBLIC = app.isPackaged ? process.env.DIST : path.join(process.env.DIST, '../public')
+
+const preload = path.join(__dirname, './preload.js')
+const url = process.env['VITE_DEV_SERVER_URL']
+
+app.on('ready', () => {
+  createServer()
+
+  const win = new BrowserWindow({
+    webPreferences: {
+      preload,
+
+      webSecurity: process.env.NODE_ENV === 'production',
+    },
+  })
+
+  createIPCHandler({ router: appRouter, windows: [win] })
+  ipcMain.handle('getIp', () => require('ip').address())
+  console.log(`Running from ${require('ip').address()}...`)
+
+  if (url) {
+    win.loadURL(url)
+  } else {
+    win.loadFile(path.join(process.env.DIST!, 'index.html'))
+  }
+
+  win.show()
+})
