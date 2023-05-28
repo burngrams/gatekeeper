5ce490a8ece72acf1b2148740df93d78a9535fb0
Peter Griffin <thebird@istheword.com>
2023-05-28 16:36:51 +0300

    refactor: AuditlogTable new file

--- apps/gatekeeper-desktop/client/CommandPage.tsx
+++ apps/gatekeeper-desktop/client/CommandPage.tsx
@@ -1,65 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { formIdToQRImage } from './backend.layer';
 
 import QRCode from 'qrcode';
+import { AuditlogTable } from './AuditlogTable';
 import "./CommandPage.css";
 import { Section } from './Section';
-import { json } from 'stream/consumers';
-import { AuditlogModel, OperationLogModel } from '../lib/models';
-import { trpc, wsClient } from './trpc';
-
-/**
- * takes an auditlog and returns a table JSX element
- * it takes each property of the auditlog and creates a th row
- * then it takes each operationlog and creates a tr row with td for each property
- * 
- */
-function AuditlogTable() {
-	const [auditlogRaw, setAuditlog] = useState<AuditlogModel>([])
-	const auditlog = auditlogRaw.reverse()
-
-	useEffect(() => {
-		const getAuditlog = async () => {
-			const { auditlog } = await trpc.auditlog.get.query();
-			setAuditlog(auditlog)
-		}
-		getAuditlog()
-	}, [])
-
-	useEffect(() => {
-		const disposer = wsClient.auditlog.onAdd.subscribe(undefined, {
-			onData(operationLog) {
-				setAuditlog(auditlog => [...auditlog, operationLog])
-			},
-		})
-
-		return () => disposer.unsubscribe()
-	}, [])
-
-	/* #region creating table elements jsx */
-	const item = auditlog.length ? auditlog[0] : {}
-	const ths = Object.keys(item).map(key => <th>{key}</th>)
-	const trs = auditlog.map(operationLog => {
-		const tds = Object.values(operationLog).map(value => {
-			if (typeof value === 'object') {
-				return <td>{JSON.stringify(value)}</td>
-			}
-			return <td>{value}</td>
-		})
-		return <tr>{tds}</tr>
-	})
-	/* #endregion */
-
-	return <table>
-		<thead>
-			<tr>{ths}</tr>
-		</thead>
-		<tbody>
-			{trs}
-		</tbody>
-	</table>
-}
-
 
 export function CommandPage() {
 	useEffect(() =>
